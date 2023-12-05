fe9489d9cc6750d8028603863031024663933418
locpham97 <loc.pham@codeenginestudio.com>
2023-12-05 10:59:11 +0700

    LPS-187433 Add an additional configuration for adding iso countries

--- modules/apps/address/address-impl/src/main/java/com/liferay/address/internal/osgi/commands/PortalAddressOSGiCommands.java
+++ modules/apps/address/address-impl/src/main/java/com/liferay/address/internal/osgi/commands/PortalAddressOSGiCommands.java
@@ -5,8 +5,10 @@
 
 package com.liferay.address.internal.osgi.commands;
 
+import com.liferay.address.internal.configuration.AdditionalCountriesConfiguration;
 import com.liferay.address.internal.util.CompanyCountriesUtil;
 import com.liferay.osgi.util.osgi.commands.OSGiCommands;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
@@ -14,6 +16,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Country;
 import com.liferay.portal.kernel.model.Release;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.CountryLocalService;
 import com.liferay.portal.kernel.service.RegionLocalService;
@@ -70,15 +73,35 @@ public class PortalAddressOSGiCommands implements OSGiCommands {
 		JSONArray countriesJSONArray = CompanyCountriesUtil.getJSONArray(
 			"com/liferay/address/dependencies/countries.json");
 
-		if (PropsValues.ISO_COUNTRIES_ENABLED) {
-			JSONArray additionalCountriesJSONArray = _getJSONArray(
-				"com/liferay/address/dependencies/additional-countries.json");
+		try {
+			AdditionalCountriesConfiguration additionalCountriesConfiguration =
+				_configurationProvider.getCompanyConfiguration(
+					AdditionalCountriesConfiguration.class,
+					company.getCompanyId());
 
-			for (int i = 0; i < additionalCountriesJSONArray.length(); i++) {
-				JSONObject countryJSONObject =
-					additionalCountriesJSONArray.getJSONObject(i);
+			if (additionalCountriesConfiguration.enableISO3166Countries()) {
+				JSONArray additionalCountriesJSONArray =
+					CompanyCountriesUtil.getJSONArray(
+						"com/liferay/address/dependencies" +
+							"/additional-iso-3166-countries.json");
 
-				countriesJSONArray.put(countryJSONObject);
+				for (int i = 0; i < additionalCountriesJSONArray.length();
+					 i++) {
+
+					JSONObject countryJSONObject =
+						additionalCountriesJSONArray.getJSONObject(i);
+
+					String name = countryJSONObject.getString("name");
+
+					if (!countryNames.contains(name)) {
+						countriesJSONArray.put(countryJSONObject);
+					}
+				}
+			}
+		}
+		catch (ConfigurationException configurationException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(configurationException);
 			}
 		}
 
@@ -117,31 +140,14 @@ public class PortalAddressOSGiCommands implements OSGiCommands {
 		}
 	}
 
-	private void _processAdditionalCountries(Company company) throws Exception {
-		JSONArray additionalCountriesJSONArray =
-			CompanyCountriesUtil.getJSONArray(
-				"com/liferay/address/dependencies/additional-countries.json");
-
-		for (int i = 0; i < additionalCountriesJSONArray.length(); i++) {
-			JSONObject countryJSONObject =
-				additionalCountriesJSONArray.getJSONObject(i);
-
-			try {
-				CompanyCountriesUtil.addCountry(
-					company, countryJSONObject, _countryLocalService,
-					_regionLocalService);
-			}
-			catch (Exception exception) {
-				_log.error(exception);
-			}
-		}
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		PortalAddressOSGiCommands.class);
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
 
 	@Reference
 	private CountryLocalService _countryLocalService;
